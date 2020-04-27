ab7e0999e3aee362ac1eb4bda37523acb2dfad9f
dxMaria <mariya.nikulina@devexpress.com>
2020-04-27 16:51:08 +0300

    commands

--- CS/RichEditCustomCopyPaste/CustomCommands.cs
+++ CS/RichEditCustomCopyPaste/CustomCommands.cs
@@ -5,29 +5,36 @@ using DevExpress.XtraRichEdit.Commands;
 using DevExpress.XtraRichEdit.Commands.Internal;
 using DevExpress.XtraRichEdit.Export;
 using DevExpress.XtraRichEdit.Export.Html;
+using DevExpress.XtraRichEdit.Utils;
 using DevExpress.Office.Commands.Internal;
 using DevExpress.Office.Utils;
 
-namespace RichEditCustomCopyPaste {
-    public class CustomCopySelectionCommand : CopySelectionCommand {
+namespace RichEditCustomCopyPaste
+{
+    public class CustomCopySelectionCommand : CopySelectionCommand
+    {
         public CustomCopySelectionCommand(IRichEditControl control)
-            : base(control) {
+            : base(control)
+        {
         }
-        protected override void ExecuteCore() {
+        protected override void ExecuteCore()
+        {
             RichEditControl richEditControl = (RichEditControl)Control;
             richEditControl.BeforeExport += OnBeforeExport;
             string htmlForClipboard = String.Empty;
 
             richEditControl.Options.Export.Html.ExportRootTag = ExportRootTag.Html;
-            
-            try {
+
+            try
+            {
                 string html = Control.Document.GetHtmlText(Control.Document.Selection, new CustomUriProvider(), richEditControl.Options.Export.Html);
                 htmlForClipboard = CF_HTMLHelper.GetHtmlClipboardFormat(html);
             }
-            finally {
+            finally
+            {
                 richEditControl.BeforeExport -= OnBeforeExport;
             }
-            
+
             DataObject data = new DataObject();
             data.SetData(OfficeDataFormats.Rtf, richEditControl.Document.GetRtfText(richEditControl.Document.Selection));
             data.SetData(OfficeDataFormats.UnicodeText, richEditControl.Document.GetText(richEditControl.Document.Selection));
@@ -36,34 +43,42 @@ namespace RichEditCustomCopyPaste {
             Clipboard.SetDataObject(data, false);
         }
 
-        void OnBeforeExport(object sender, BeforeExportEventArgs e) {
+        void OnBeforeExport(object sender, BeforeExportEventArgs e)
+        {
             HtmlDocumentExporterOptions exporterOptions = e.Options as HtmlDocumentExporterOptions;
 
-            if (exporterOptions != null) {
+            if (exporterOptions != null)
+            {
                 exporterOptions.CssPropertiesExportType = CssPropertiesExportType.Inline;
                 exporterOptions.ExportRootTag = ExportRootTag.Body;
                 exporterOptions.EmbedImages = false; // To delegate handling into a CustomUriProvider
             }
         }
     }
-    public class CustomPasteSelectionCommand : PasteSelectionCommand {
+    public class CustomPasteSelectionCommand : PasteSelectionCommand
+    {
         public CustomPasteSelectionCommand(IRichEditControl control)
-            : base(control) {
+            : base(control)
+        {
 
         }
 
-        protected override RichEditCommand CreateInsertObjectCommand() {
+        protected override RichEditCommand CreateInsertObjectCommand()
+        {
             return new CustomPasteSelectionCoreCommand(base.Control, new ClipboardPasteSource());
         }
     }
 
-    public class CustomPasteSelectionCoreCommand : PasteSelectionCoreCommand {
+    public class CustomPasteSelectionCoreCommand : PasteSelectionCoreCommand
+    {
         public CustomPasteSelectionCoreCommand(IRichEditControl control, PasteSource pasteSource)
-            : base(control, pasteSource) {
+            : base(control, pasteSource)
+        {
 
         }
 
-        public override void ForceExecute(DevExpress.Utils.Commands.ICommandUIState state) {
+        public override void ForceExecute(DevExpress.Utils.Commands.ICommandUIState state)
+        {
             Control.Document.InsertText(Control.Document.CaretPosition, Clipboard.GetText());
         }
     }
